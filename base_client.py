de829b1b5ea2b077560d7132ce9708788d80e0b8
Kubon1999 <kuba.wolodzko@gmail.com>
2022-04-25 20:53:05 +0200

    p2p

--- base_client.py
+++ base_client.py
@@ -6,8 +6,8 @@ SIZE_OF_HEADER = 64
 IP = "0.0.0.0"
 PORT = 3000
 ADDRESS = (IP, PORT)
-#CLIENT_DISCONNECT_MESSAGE = "/exit"
 FORMAT_OF_MESSAGE_IN_SOCKET = "utf-8" 
+CLIENT_DISCONNECT_MESSAGE = "/exit"
 
 def client_connection(connection, address):
     print(f"{address} connection established")
@@ -19,31 +19,39 @@ def client_connection(connection, address):
             message_size = int(message_size)
             message = connection.recv(message_size).decode(FORMAT_OF_MESSAGE_IN_SOCKET)
             if message == CLIENT_DISCONNECT_MESSAGE:
+                print("Client disconnnected.")
                 connected = False
 
             print(f"[{address}] {message}")
         
     connection.close()
 
+def send(client,message):
+    message = message.encode(FORMAT_OF_MESSAGE_IN_SOCKET)
+    length_of_message = len(message) #check this one why do we store the length in string then int
+    length_of_message = str(length_of_message).encode(FORMAT_OF_MESSAGE_IN_SOCKET)
+    length_of_message += b' ' * (SIZE_OF_HEADER - len(length_of_message))#lets add the few bytes to make the size of header = 64, so if the 'length_of_message' is 24  we need to add 64-24=40bytes
+    #this is because we make server read EXACTLY 64 bytes so now we must send EXACTLY 64 bytes
+    client.send(length_of_message)
+    #print(f"Sending: {message}")
+    client.send(message)
+    #print(f"Send!")
+
 
 def base_client_start():
     print("Starting the base client...")
-    client_running = True
 
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #SOCK_STREAM because the order of the data that is sent is important 
     client.bind(ADDRESS)
 
     client.listen()
-    print("Client running...")
-  #  while client_running:
+    print("Base client running...")
     connection, address = client.accept()
-        #client_thread = threading.Thread(target=client_connection, args=(connection, address)) #args method
-    client_connection(connection,address)
-        #client_thread.start()
-        #print(f"Clients connected: {threading.active_count() - 1}") #-1 becausue one thread is our sever thread 
-
-
-        
-
+    connection_thread = threading.Thread(target=client_connection, args=(connection,"client"))
+    connection_thread.start()
+    #while recieving thread is up lets send messages
+    while True:
+        message = input()
+        send(connection, message)
 
 base_client_start()
--- client.py
+++ client.py
@@ -9,23 +9,45 @@ SERVER_ADDRESS = (SERVER_IP, SERVER_PORT)
 CLIENT_DISCONNECT_MESSAGE = "/exit"
 FORMAT_OF_MESSAGE_IN_SOCKET = "utf-8"
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #SOCK_STREAM because the order of the data that is sent is important 
-print("Client started...")
-print("Trying to connect...")
-client.connect(SERVER_ADDRESS)
-print("Connected!")
-
-def send(message):
+def send(client,message):
     message = message.encode(FORMAT_OF_MESSAGE_IN_SOCKET)
     length_of_message = len(message) #check this one why do we store the length in string then int
     length_of_message = str(length_of_message).encode(FORMAT_OF_MESSAGE_IN_SOCKET)
     length_of_message += b' ' * (SIZE_OF_HEADER - len(length_of_message))#lets add the few bytes to make the size of header = 64, so if the 'length_of_message' is 24  we need to add 64-24=40bytes
     #this is because we make server read EXACTLY 64 bytes so now we must send EXACTLY 64 bytes
     client.send(length_of_message)
-    print(f"Sending: {message}")
+    #print(f"Sending: {message}")
     client.send(message)
-    print(f"Send!")
+    #print(f"Send!")
 
+def recieve_message(connection, address):
+    print(f"{address} connection established")
+    connected = True
 
+    while connected:
+        message_size = connection.recv(SIZE_OF_HEADER).decode(FORMAT_OF_MESSAGE_IN_SOCKET)
+        if message_size:
+            message_size = int(message_size)
+            message = connection.recv(message_size).decode(FORMAT_OF_MESSAGE_IN_SOCKET)
+            if message == CLIENT_DISCONNECT_MESSAGE:
+                connected = False
 
-send("Hello world!")
+            print(f"[{address}] {message}")
+        
+    connection.close()
+
+def client_start():
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #SOCK_STREAM because the order of the data that is sent is important 
+    print("Client started...")
+    print("Trying to connect...")
+    client.connect(SERVER_ADDRESS)
+    print("Connected!")
+    recieve_message_thread = threading.Thread(target=recieve_message, args=(client, "base_client"))
+    recieve_message_thread.start()
+
+    #while recieving thread is up lets send messages
+    while True:
+        message = input()
+        send(client, message)
+
+client_start()
